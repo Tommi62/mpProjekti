c91f7b92708e095732e488e86fca827451404d4b
andreisamuellee <54439182+andreisamuellee@users.noreply.github.com>
2021-04-19 14:13:33 +0300

    login and register base

--- src/views/Login.js
+++ src/views/Login.js
@@ -1,11 +1,19 @@
-import {Typography} from '@material-ui/core';
+import LoginForm from '../components/LoginForm';
+import RegisterForm from '../components/RegisterForm';
+import {Button} from '@material-ui/core';
+import {useState} from 'react';
 
 const Login = () => {
+  const [toggle, setToggle] = useState(true);
+
+  const showHide = () => {
+    setToggle(!toggle);
+  };
+
   return (
     <>
-      <Typography component="h1" variant="h2" gutterBottom align="center">
-        Login
-      </Typography>
+      { toggle ? <LoginForm /> : <RegisterForm setToggle={setToggle} />}
+      <Button onClick={showHide}>{toggle ? 'or register' : 'or login'}</Button>
     </>
   );
 };
