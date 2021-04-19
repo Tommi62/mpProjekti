e89a105d65852b1fe8a162b631aedf7112510b93
Tommi <tommi.vainio21@gmail.com>
2021-04-19 18:16:12 +0300

    Map shows your location by clicking it

--- src/components/Map.js
+++ src/components/Map.js
@@ -1,12 +1,39 @@
-import {MapContainer, TileLayer, Marker, Popup} from 'react-leaflet';
+/* eslint-disable require-jsdoc */
+import {useState} from 'react';
+import {
+  MapContainer,
+  TileLayer,
+  Marker,
+  Popup,
+  useMapEvents,
+} from 'react-leaflet';
 
 const Map = () => {
+  const LocationMarker = () => {
+    const [position, setPosition] = useState(null);
+    const map = useMapEvents({
+      click() {
+        map.locate();
+      },
+      locationfound(e) {
+        setPosition(e.latlng);
+        map.flyTo(e.latlng, map.getZoom());
+      },
+    });
+
+    return position === null ? null : (
+      <Marker position={position}>
+        <Popup>You are here</Popup>
+      </Marker>
+    );
+  };
+
   return (
     <>
       <MapContainer
         center={[51.505, -0.09]}
-        zoom={13}
-        scrollWheelZoom={false}
+        zoom={15}
+        scrollWheelZoom={true}
         style={{height: '100%'}}
       >
         <TileLayer
@@ -18,6 +45,7 @@ const Map = () => {
             A pretty CSS3 popup. <br /> Easily customizable.
           </Popup>
         </Marker>
+        <LocationMarker />
       </MapContainer>
     </>
   );
