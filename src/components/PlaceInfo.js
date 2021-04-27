86d0533b120cce6234f2bd31583ee57ea03cb9c4
andreisamuellee <54439182+andreisamuellee@users.noreply.github.com>
2021-04-27 15:29:58 +0300

    nav, home and comment styling

--- src/components/PlaceInfo.js
+++ src/components/PlaceInfo.js
@@ -1,5 +1,5 @@
 /* eslint-disable max-len */
-import {Grid, Typography} from '@material-ui/core';
+import {Box, Card, CardHeader, CardMedia, Divider, Tabs, Typography} from '@material-ui/core';
 /* eslint-disable comma-dangle */
 /* eslint-disable indent */
 import {Avatar} from '@material-ui/core';
@@ -10,9 +10,60 @@ import {useTag} from '../hooks/ApiHooks';
 import LikeButton from './LikeButton';
 import CloseButton from './CloseButton';
 import CommentSection from './CommentSection';
+import {makeStyles} from '@material-ui/core/styles';
+import LocationOnIcon from '@material-ui/icons/LocationOn';
+
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    maxWidth: 345,
+  },
+  media: {
+    height: 0,
+    paddingTop: '56.25%', // 16:9
+  },
+  expand: {
+    transform: 'rotate(0deg)',
+    marginLeft: 'auto',
+    transition: theme.transitions.create('transform', {
+      duration: theme.transitions.duration.shortest,
+    }),
+  },
+  expandOpen: {
+    transform: 'rotate(180deg)',
+  },
+  avatar: {
+  },
+  address: {
+    display: 'flex',
+    alignItems: 'center',
+    textTransform: 'capitalize',
+    marginRight: 'auto',
+    padding: '9px'
+  },
+  addressIcon: {
+    position: 'absolute',
+    transform: 'translate(-50%, -50%)',
+  },
+  addressBox: {
+    display: 'flex',
+    alignItems: 'center'
+  },
+  likeBtn: {
+    marginLeft: 'auto'
+  },
+  desc: {
+    padding: '16px',
+    maxHeight: '100%',
+    maxWidth: '100%',
+    whiteSpace: 'pre-line',
+  },
+}));
 
 const PlaceInfo = ({data, user, onChange}) => {
+  const classes = useStyles();
   const [avatar, setAvatar] = useState('');
+  const [myAvatar, setMyAvatar] = useState('');
   const {getTag} = useTag();
 
   useEffect(() => {
@@ -31,48 +82,65 @@ const PlaceInfo = ({data, user, onChange}) => {
     })();
   }, [data]);
 
+  useEffect(() => {
+    (async () => {
+      try {
+        const result = await getTag('avatar_' + user.user_id);
+        if (result.length > 0) {
+          const image = result.pop().filename;
+          setMyAvatar(uploadsUrl + image);
+        } else {
+          setMyAvatar('');
+        }
+      } catch (e) {
+        console.log(e.message);
+      }
+    })();
+  }, [data]);
+
   return (
     <>
-      <Grid container alignItems="center" style={{margin: '1rem'}}>
-        <Avatar
-          variant={'round'}
-          src={avatar}
-          style={{marginRight: '0.5rem'}}
-        />
-        <Typography variant="subtitle2" style={{width: '22.5rem'}}>
-          {data.username}
-        </Typography>
-        <CloseButton onChange={onChange} />
-      </Grid>
-      <Grid container direction="column" justify="center">
-        <Grid item>
-          <Typography component="h1" variant="h2" gutterBottom align="center">
-            {data.title}
-          </Typography>
-        </Grid>
-
-        <img
-          src={uploadsUrl + data.file}
-          alt={data.title}
-          style={{
-            width: '25rem',
-            margin: 'auto',
-          }}
-        />
-        <LikeButton data={data} user={user} />
-
-        <Grid item>
-          <Typography gutterBottom align="center">
-            {data.address} {data.city}
-          </Typography>
-        </Grid>
-        <Grid item>
-          <Typography gutterBottom align="center">
-            {data.description}
-          </Typography>
-        </Grid>
-      </Grid>
-      <CommentSection data={data} user={user} />
+      <Tabs
+        orientation="vertical"
+        variant="scrollable">
+        <Card variant="scrollable">
+          <CardHeader
+            avatar={
+              <Avatar
+                variant={'round'}
+                src={avatar}
+                style={{marginRight: '0.5rem'}}
+              />
+            }
+            action={
+              <CloseButton onChange={onChange} />
+            }
+            title={data.title}
+            subheader={data.username}
+          />
+          <CardMedia
+            className={classes.media}
+            image={uploadsUrl + data.file}
+          />
+          <Box className={classes.addressBox}>
+            <Box className={classes.address}>
+              <LocationOnIcon color="disabled" />
+              <Typography variant="body2" color="textSecondary" component="p">
+                {data.address}, {data.city}
+              </Typography>
+            </Box>
+            <LikeButton data={data} user={user} />
+          </Box>
+          <Divider />
+          <Box>
+            <Typography variant="body1" color="textPrimary" component="p" className={classes.desc}>
+              {data.description}
+            </Typography>
+          </Box>
+          <Divider />
+          <CommentSection data={data} user={user} avatar={myAvatar} />
+        </Card>
+      </Tabs>
     </>
   );
 };
