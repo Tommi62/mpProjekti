84edc8d6d9c3f1afbc257d173d5b9b9465385b57
andreisamuellee <54439182+andreisamuellee@users.noreply.github.com>
2021-05-05 17:30:08 +0300

    forms done

--- src/views/Login.js
+++ src/views/Login.js
@@ -2,6 +2,7 @@ import LoginForm from '../components/LoginForm';
 import RegisterForm from '../components/RegisterForm';
 import {Button, Grid} from '@material-ui/core';
 import {useState} from 'react';
+import Card from '@material-ui/core/Card';
 
 const Login = () => {
   const [toggle, setToggle] = useState(true);
@@ -12,10 +13,18 @@ const Login = () => {
 
   return (
     <>
-      {toggle ? <LoginForm /> : <RegisterForm setToggle={setToggle} />}
       <Grid container justify="center">
-        <Button justify='center'
-          onClick={showHide}>{toggle ? 'or register' : 'or login'}</Button>
+        <Card style={{
+          margin: '5rem 2rem 3rem 2rem',
+          padding: '3rem',
+          maxWidth: '18rem',
+        }}>
+          {toggle ? <LoginForm /> : <RegisterForm setToggle={setToggle} />}
+          <Grid container justify="center">
+            <Button justify='center'
+              onClick={showHide}>{toggle ? 'or register' : 'or login'}</Button>
+          </Grid>
+        </Card>
       </Grid>
     </>
   );
